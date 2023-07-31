9d0cb645a6960800cd744d93fb58dbee2357ed55
Kamila Queiroz <kamilapaqueiroz@gmail.com>
2023-07-31 20:08:59 -0300

    Initial commit

---
+++ app-banco/src/test/java/me/dio/appbanco/AppBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.appbanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
